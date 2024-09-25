280e4c3a6fbe3a555807aedb80be0922a9b25186
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-09-25 23:17:08 +0200

    Added database and tables, Added 5 rooms into the Room table
    -There are 5 rooms in this hotel only (according to the test data document)

--- HotelSystem/HotelSystem/Data/DB.cs
+++ HotelSystem/HotelSystem/Data/DB.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApp1.Data
+namespace HotelSystem.Data
 {
     internal class DB
     {
--- HotelSystem/HotelSystem/Form1.Designer.cs
+++ HotelSystem/HotelSystem/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace HotelSystem
 {
     partial class Form1
     {
