86175b6f672ac5e71b647feaa91bf37a3815805b
lordweri <werimasao@gmail.com>
2024-09-29 12:52:35 +0200

    updating classes

--- HotelSystem/HotelSystem/Business/DateRange.cs
+++ HotelSystem/HotelSystem/Business/DateRange.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelSystem
 {
-   public class DateRange
+    public class DateRange
     {
         public DateTime Start;
         public DateTime End;
+
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public int GetNumberOfDays()
+        {
+            return (End - Start).Days;
+        }
     }
 }
--- HotelSystem/HotelSystem/Business/Room.cs
+++ HotelSystem/HotelSystem/Business/Room.cs
@@ -11,5 +11,22 @@ namespace HotelSystem
         private string roomID;
         private double rate;
         private Boolean availability;
+
+        public Room(string ID,double price)
+        {
+            roomID = ID;
+            rate = price;
+            availability = true;
+        }
+        
+        public void changeAvailability(Boolean available)
+        {
+            availability = available;
+        }
+
+        
+        public double getRate() { return rate; }
+        public string getRoomID() { return roomID; }
+        public Boolean getAvailability() { return availability; }
     }
 }
