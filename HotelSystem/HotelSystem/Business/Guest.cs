bc380f6f7265f64682b660c734c13cb43897099e
lordweri <werimasao@gmail.com>
2024-09-29 14:07:50 +0200

    Updated guest class

--- HotelSystem/HotelSystem/Business/Guest.cs
+++ HotelSystem/HotelSystem/Business/Guest.cs
@@ -8,5 +8,45 @@ namespace HotelSystem
 {
     public class Guest
     {
+        private string Name;
+        private string guestID;
+        private string email;
+        private string telephone;
+        private Boolean check;
+
+
+        public Guest(string name, string Email, string phone)
+        {
+            this.Name = name;
+            this.email = Email;
+            this.telephone = phone;
+            this.guestID = GenerateGuestID(name);
+        }
+        #region utility methods
+        private static string GenerateGuestID(string firstName)
+        {
+
+            string firstPart = firstName.Length >= 3 ? firstName.Substring(0, 3).ToUpper() : firstName.PadRight(3, 'X').ToUpper();
+            string randomLetters = GenerateRandomLetters(3);
+            string randomNumbers = GenerateRandomNumbers(3);
+            return firstPart + randomLetters + randomNumbers;
+        }
+
+        private static string GenerateRandomLetters(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private static string GenerateRandomNumbers(int length)
+        {
+            Random random = new Random();
+            return new string(Enumerable.Repeat("0123456789", length)
+                                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        #endregion
     }
+
 }
