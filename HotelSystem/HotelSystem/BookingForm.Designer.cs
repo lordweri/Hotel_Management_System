a61e7cb272fff52cca5aa0bf0fe256ddfebe9309
Caleb Brown <BRWCAL007@myuct.ac.za>
2024-10-01 01:45:26 +0200

    Updated the BookingForm Code and Form Design, has a few errors but should meet most of the criteria for the booking functionalities.

--- HotelSystem/HotelSystem/BookingForm.Designer.cs
+++ HotelSystem/HotelSystem/BookingForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.cmbGuest = new System.Windows.Forms.ComboBox();
             this.guestBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.hotelDatabaseDataSet = new HotelSystem.HotelDatabaseDataSet();
             this.cmbRoom = new System.Windows.Forms.ComboBox();
@@ -37,31 +36,36 @@
             this.dtpCheckIn = new System.Windows.Forms.DateTimePicker();
             this.dtpCheckOut = new System.Windows.Forms.DateTimePicker();
             this.btnSaveBooking = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblGuestName = new System.Windows.Forms.Label();
+            this.lblRoom = new System.Windows.Forms.Label();
+            this.lblCheckIn = new System.Windows.Forms.Label();
+            this.lblCheckOut = new System.Windows.Forms.Label();
             this.guestTableAdapter = new HotelSystem.HotelDatabaseDataSetTableAdapters.GuestTableAdapter();
             this.roomTableAdapter = new HotelSystem.HotelDatabaseDataSetTableAdapters.RoomTableAdapter();
             this.hotelDatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.txtGuestName = new System.Windows.Forms.TextBox();
-            this.txtRoomNumber = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.errorLabel = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblAdults = new System.Windows.Forms.Label();
+            this.lblChildren = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.nudAdults = new System.Windows.Forms.NumericUpDown();
+            this.nudChildren = new System.Windows.Forms.NumericUpDown();
+            this.dgvBookings = new System.Windows.Forms.DataGridView();
+            this.hotelDatabaseDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.guestBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.roomBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudChildren)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSetBindingSource1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cmbGuest
-            // 
-            this.cmbGuest.DataSource = this.guestBindingSource;
-            this.cmbGuest.DisplayMember = "GuestID";
-            this.cmbGuest.FormattingEnabled = true;
-            this.cmbGuest.Location = new System.Drawing.Point(369, 78);
-            this.cmbGuest.Name = "cmbGuest";
-            this.cmbGuest.Size = new System.Drawing.Size(200, 24);
-            this.cmbGuest.TabIndex = 0;
             // 
             // guestBindingSource
             // 
@@ -78,7 +82,7 @@
             this.cmbRoom.DataSource = this.roomBindingSource;
             this.cmbRoom.DisplayMember = "RoomNumber";
             this.cmbRoom.FormattingEnabled = true;
-            this.cmbRoom.Location = new System.Drawing.Point(367, 187);
+            this.cmbRoom.Location = new System.Drawing.Point(118, 258);
             this.cmbRoom.Name = "cmbRoom";
             this.cmbRoom.Size = new System.Drawing.Size(200, 24);
             this.cmbRoom.TabIndex = 1;
@@ -90,21 +94,21 @@
             // 
             // dtpCheckIn
             // 
-            this.dtpCheckIn.Location = new System.Drawing.Point(179, 321);
+            this.dtpCheckIn.Location = new System.Drawing.Point(118, 333);
             this.dtpCheckIn.Name = "dtpCheckIn";
             this.dtpCheckIn.Size = new System.Drawing.Size(200, 22);
             this.dtpCheckIn.TabIndex = 2;
             // 
             // dtpCheckOut
             // 
-            this.dtpCheckOut.Location = new System.Drawing.Point(179, 452);
+            this.dtpCheckOut.Location = new System.Drawing.Point(118, 401);
             this.dtpCheckOut.Name = "dtpCheckOut";
             this.dtpCheckOut.Size = new System.Drawing.Size(200, 22);
             this.dtpCheckOut.TabIndex = 3;
             // 
             // btnSaveBooking
             // 
-            this.btnSaveBooking.Location = new System.Drawing.Point(367, 618);
+            this.btnSaveBooking.Location = new System.Drawing.Point(378, 618);
             this.btnSaveBooking.Name = "btnSaveBooking";
             this.btnSaveBooking.Size = new System.Drawing.Size(202, 51);
             this.btnSaveBooking.TabIndex = 4;
@@ -112,41 +116,41 @@
             this.btnSaveBooking.UseVisualStyleBackColor = true;
             this.btnSaveBooking.Click += new System.EventHandler(this.btnSaveBooking_Click);
             // 
-            // label1
+            // lblGuestName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Guest:";
+            this.lblGuestName.AutoSize = true;
+            this.lblGuestName.Location = new System.Drawing.Point(24, 48);
+            this.lblGuestName.Name = "lblGuestName";
+            this.lblGuestName.Size = new System.Drawing.Size(45, 16);
+            this.lblGuestName.TabIndex = 5;
+            this.lblGuestName.Text = "Guest:";
             // 
-            // label2
+            // lblRoom
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 187);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Room:";
+            this.lblRoom.AutoSize = true;
+            this.lblRoom.Location = new System.Drawing.Point(22, 266);
+            this.lblRoom.Name = "lblRoom";
+            this.lblRoom.Size = new System.Drawing.Size(47, 16);
+            this.lblRoom.TabIndex = 6;
+            this.lblRoom.Text = "Room:";
             // 
-            // label3
+            // lblCheckIn
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 327);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 16);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Check In Date:";
+            this.lblCheckIn.AutoSize = true;
+            this.lblCheckIn.Location = new System.Drawing.Point(14, 333);
+            this.lblCheckIn.Name = "lblCheckIn";
+            this.lblCheckIn.Size = new System.Drawing.Size(93, 16);
+            this.lblCheckIn.TabIndex = 7;
+            this.lblCheckIn.Text = "Check In Date:";
             // 
-            // label4
+            // lblCheckOut
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 458);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 16);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Check Out Date:";
+            this.lblCheckOut.AutoSize = true;
+            this.lblCheckOut.Location = new System.Drawing.Point(14, 406);
+            this.lblCheckOut.Name = "lblCheckOut";
+            this.lblCheckOut.Size = new System.Drawing.Size(103, 16);
+            this.lblCheckOut.TabIndex = 8;
+            this.lblCheckOut.Text = "Check Out Date:";
             // 
             // guestTableAdapter
             // 
@@ -163,67 +167,168 @@
             // 
             // txtGuestName
             // 
-            this.txtGuestName.Location = new System.Drawing.Point(179, 79);
+            this.txtGuestName.Location = new System.Drawing.Point(118, 48);
             this.txtGuestName.Name = "txtGuestName";
-            this.txtGuestName.Size = new System.Drawing.Size(100, 22);
+            this.txtGuestName.Size = new System.Drawing.Size(200, 22);
             this.txtGuestName.TabIndex = 9;
             // 
-            // txtRoomNumber
+            // txtAddress
             // 
-            this.txtRoomNumber.Location = new System.Drawing.Point(179, 184);
-            this.txtRoomNumber.Name = "txtRoomNumber";
-            this.txtRoomNumber.Size = new System.Drawing.Size(100, 22);
-            this.txtRoomNumber.TabIndex = 10;
+            this.txtAddress.Location = new System.Drawing.Point(118, 120);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 22);
+            this.txtAddress.TabIndex = 10;
             // 
             // errorLabel
             // 
             this.errorLabel.AutoSize = true;
-            this.errorLabel.Location = new System.Drawing.Point(708, 653);
+            this.errorLabel.Location = new System.Drawing.Point(24, 653);
             this.errorLabel.Name = "errorLabel";
             this.errorLabel.Size = new System.Drawing.Size(0, 16);
             this.errorLabel.TabIndex = 11;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(24, 187);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(49, 16);
+            this.lblPhone.TabIndex = 12;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(24, 123);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(61, 16);
+            this.lblAddress.TabIndex = 13;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblAdults
+            // 
+            this.lblAdults.AutoSize = true;
+            this.lblAdults.Location = new System.Drawing.Point(24, 489);
+            this.lblAdults.Name = "lblAdults";
+            this.lblAdults.Size = new System.Drawing.Size(47, 16);
+            this.lblAdults.TabIndex = 14;
+            this.lblAdults.Text = "Adults:";
+            // 
+            // lblChildren
+            // 
+            this.lblChildren.AutoSize = true;
+            this.lblChildren.Location = new System.Drawing.Point(22, 558);
+            this.lblChildren.Name = "lblChildren";
+            this.lblChildren.Size = new System.Drawing.Size(56, 16);
+            this.lblChildren.TabIndex = 15;
+            this.lblChildren.Text = "Children";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(118, 180);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(200, 22);
+            this.txtPhone.TabIndex = 16;
+            // 
+            // nudAdults
+            // 
+            this.nudAdults.Location = new System.Drawing.Point(118, 482);
+            this.nudAdults.Name = "nudAdults";
+            this.nudAdults.Size = new System.Drawing.Size(200, 22);
+            this.nudAdults.TabIndex = 17;
+            // 
+            // nudChildren
+            // 
+            this.nudChildren.Location = new System.Drawing.Point(118, 558);
+            this.nudChildren.Name = "nudChildren";
+            this.nudChildren.Size = new System.Drawing.Size(200, 22);
+            this.nudChildren.TabIndex = 18;
+            // 
+            // dgvBookings
+            // 
+            this.dgvBookings.AutoGenerateColumns = false;
+            this.dgvBookings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookings.DataSource = this.hotelDatabaseDataSetBindingSource1;
+            this.dgvBookings.Location = new System.Drawing.Point(617, 145);
+            this.dgvBookings.Name = "dgvBookings";
+            this.dgvBookings.RowHeadersWidth = 51;
+            this.dgvBookings.RowTemplate.Height = 24;
+            this.dgvBookings.Size = new System.Drawing.Size(399, 390);
+            this.dgvBookings.TabIndex = 19;
+            // 
+            // hotelDatabaseDataSetBindingSource1
+            // 
+            this.hotelDatabaseDataSetBindingSource1.DataSource = this.hotelDatabaseDataSet;
+            this.hotelDatabaseDataSetBindingSource1.Position = 0;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(378, 53);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 20;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(428, 47);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(152, 22);
+            this.txtEmail.TabIndex = 21;
             // 
             // BookingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 725);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.dgvBookings);
+            this.Controls.Add(this.nudChildren);
+            this.Controls.Add(this.nudAdults);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblChildren);
+            this.Controls.Add(this.lblAdults);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblPhone);
             this.Controls.Add(this.errorLabel);
-            this.Controls.Add(this.txtRoomNumber);
+            this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtGuestName);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCheckOut);
+            this.Controls.Add(this.lblCheckIn);
+            this.Controls.Add(this.lblRoom);
+            this.Controls.Add(this.lblGuestName);
             this.Controls.Add(this.btnSaveBooking);
             this.Controls.Add(this.dtpCheckOut);
             this.Controls.Add(this.dtpCheckIn);
             this.Controls.Add(this.cmbRoom);
-            this.Controls.Add(this.cmbGuest);
             this.Name = "BookingForm";
             this.Text = "BookingForm";
-            this.Load += new System.EventHandler(this.BookingForm_Load_1);
+            this.Load += new System.EventHandler(this.BookingForm_Load);
             this.Click += new System.EventHandler(this.BookingForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.guestBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.roomBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudChildren)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDatabaseDataSetBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox cmbGuest;
         private System.Windows.Forms.ComboBox cmbRoom;
         private System.Windows.Forms.DateTimePicker dtpCheckIn;
         private System.Windows.Forms.DateTimePicker dtpCheckOut;
         private System.Windows.Forms.Button btnSaveBooking;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblGuestName;
+        private System.Windows.Forms.Label lblRoom;
+        private System.Windows.Forms.Label lblCheckIn;
+        private System.Windows.Forms.Label lblCheckOut;
         private HotelDatabaseDataSet hotelDatabaseDataSet;
         private System.Windows.Forms.BindingSource guestBindingSource;
         private HotelDatabaseDataSetTableAdapters.GuestTableAdapter guestTableAdapter;
@@ -231,7 +336,18 @@
         private HotelDatabaseDataSetTableAdapters.RoomTableAdapter roomTableAdapter;
         private System.Windows.Forms.BindingSource hotelDatabaseDataSetBindingSource;
         private System.Windows.Forms.TextBox txtGuestName;
-        private System.Windows.Forms.TextBox txtRoomNumber;
+        private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblAdults;
+        private System.Windows.Forms.Label lblChildren;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.NumericUpDown nudAdults;
+        private System.Windows.Forms.NumericUpDown nudChildren;
+        private System.Windows.Forms.DataGridView dgvBookings;
+        private System.Windows.Forms.BindingSource hotelDatabaseDataSetBindingSource1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
--- HotelSystem/HotelSystem/BookingForm.cs
+++ HotelSystem/HotelSystem/BookingForm.cs
@@ -13,64 +13,172 @@ namespace HotelSystem
 {
     public partial class BookingForm : Form
     {
+        
         public BookingForm()
         {
             InitializeComponent();
+            
         }
+
         private void BookingForm_Load(object sender, EventArgs e)
         {
-            // Load data into the combo boxes and data grid view
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            // Loading data from Room and Guest tables into the ComboBoxes
             this.roomTableAdapter.Fill(this.hotelDatabaseDataSet.Room);
             this.guestTableAdapter.Fill(this.hotelDatabaseDataSet.Guest);
-
         }
 
         private void btnSaveBooking_Click(object sender, EventArgs e)
         {
-            // Validate required fields
-            if (string.IsNullOrEmpty(txtGuestName.Text) || string.IsNullOrEmpty(txtRoomNumber.Text) || dtpCheckIn.Value == DateTime.MinValue || dtpCheckOut.Value == DateTime.MinValue)
-            {
-                errorLabel.Text = "Please fill in all required fields.";
+            if (!ValidateInputs())
                 return;
-            }
-            // Save new booking information
+
             try
             {
-                var bookingTableAdapter = new BookingTableAdapter();
-                bookingTableAdapter.Update(hotelDatabaseDataSet.Booking);
-                var newBooking = hotelDatabaseDataSet.Booking.NewBookingRow();
-                newBooking.GuestID = (int)cmbGuest.SelectedValue;
-                newBooking.RoomNumber = (int)cmbRoom.SelectedValue;
-                newBooking.CheckInDate = dtpCheckIn.Value;
-                newBooking.CheckOutDate = dtpCheckOut.Value;
+                int guestId = GetOrCreateGuest();
+                if (guestId == -1)
+                    return;
 
-                hotelDatabaseDataSet.Booking.AddBookingRow(newBooking);
-                bookingTableAdapter.Update(hotelDatabaseDataSet.Booking);
+                int roomNumber = (int)cmbRoom.SelectedValue;
+                DateTime checkIn = dtpCheckIn.Value;
+                DateTime checkOut = dtpCheckOut.Value;
 
-                MessageBox.Show("Booking saved successfully!");
+                if (!IsRoomAvailable(roomNumber, checkIn, checkOut))
+                {
+                    ShowError("Selected room is not available for the chosen dates.");
+                    return;
+                }
+
+                decimal totalRate = CalculateTotalRate(checkIn, checkOut);
+                decimal deposit = CalculateDeposit(totalRate);
+
+                string referenceNumber = GenerateReferenceNumber();
+
+                SaveBooking(guestId, roomNumber, checkIn, checkOut, totalRate, deposit, referenceNumber);
+
+                MessageBox.Show($"Booking saved successfully! Reference Number: {referenceNumber}");
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error saving booking: " + ex.Message);
-
-                errorLabel.Text = "Booking saved successfully!";
-
+                ShowError("Error saving booking: " + ex.Message);
             }
-            
-            
-                
-
         }
 
-        private void BookingForm_Load_1(object sender, EventArgs e)
+        private bool ValidateInputs()
         {
-            // TODO: This line of code loads data into the 'hotelDatabaseDataSet.Room' table. You can move, or remove it, as needed.
-            this.roomTableAdapter.Fill(this.hotelDatabaseDataSet.Room);
-            // TODO: This line of code loads data into the 'hotelDatabaseDataSet.Guest' table. You can move, or remove it, as needed.
-            this.guestTableAdapter.Fill(this.hotelDatabaseDataSet.Guest);
+            if (string.IsNullOrEmpty(txtGuestName.Text) || cmbRoom.SelectedValue == null ||
+                dtpCheckIn.Value == DateTime.MinValue || dtpCheckOut.Value == DateTime.MinValue)
+            {
+                ShowError("Please fill in all required fields.");
+                return false;
+            }
 
+            if (dtpCheckIn.Value >= dtpCheckOut.Value)
+            {
+                ShowError("Check-out date must be after check-in date.");
+                return false;
+            }
+
+            if (nudAdults.Value + nudChildren.Value > 4)
+            {
+                ShowError("Maximum occupancy per room is 4 people.");
+                return false;
+            }
+
+            return true;
         }
 
+        private int GetOrCreateGuest()
+        {
+            var existingGuest = hotelDatabaseDataSet.Guest.FirstOrDefault(g => g.Name == txtGuestName.Text);
+            if (existingGuest != null)
+                return existingGuest.GuestID;
+
+            try
+            {
+                var newGuest = hotelDatabaseDataSet.Guest.NewGuestRow();
+                newGuest.Name = txtGuestName.Text;
+                newGuest.Email = txtEmail.Text; // Use appropriate field for contact
+                newGuest.ContactNumber = txtPhone.Text; // Map to ContactNumber field
+                hotelDatabaseDataSet.Guest.AddGuestRow(newGuest);
+                guestTableAdapter.Update(hotelDatabaseDataSet.Guest);
+                return newGuest.GuestID;
+            }
+            catch (Exception ex)
+            {
+                ShowError("Error creating new guest: " + ex.Message);
+                return -1;
+            }
+        }
+
+        private bool IsRoomAvailable(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            var overlappingBookings = hotelDatabaseDataSet.Booking.Where(b =>
+                b.RoomNumber == roomNumber &&
+                !(b.CheckOutDate <= checkIn || b.CheckInDate >= checkOut));
+
+            return !overlappingBookings.Any();
+        }
+
+        private decimal CalculateTotalRate(DateTime checkIn, DateTime checkOut)
+        {
+            decimal totalRate = 0;
+            for (DateTime date = checkIn; date < checkOut; date = date.AddDays(1))
+            {
+                totalRate += GetRoomRateForDate(date); // Simplified, based on RoomRate
+            }
+
+            // Apply discounts for children
+            int childrenCount = (int)nudChildren.Value;
+            if (childrenCount > 0)
+            {
+                int freeChildren = Math.Min(childrenCount, 1); // One child under 5 stays free
+                int halfPriceChildren = Math.Max(0, Math.Min(childrenCount - freeChildren, 2)); // Up to two children 5-16 pay half price
+
+                totalRate -= (freeChildren * totalRate / (int)(nudAdults.Value + nudChildren.Value));
+                totalRate -= (halfPriceChildren * totalRate / (int)(nudAdults.Value + nudChildren.Value) / 2);
+            }
+
+            return totalRate;
+        }
+
+        private decimal GetRoomRateForDate(DateTime date)
+        {
+            // Assuming room rate is stored in the Room table
+            var selectedRoom = hotelDatabaseDataSet.Room.FirstOrDefault(r => r.RoomNumber == (int)cmbRoom.SelectedValue);
+            return selectedRoom?.RoomRate ?? 0;
+        }
+
+        private decimal CalculateDeposit(decimal totalRate)
+        {
+            return totalRate * 0.1m; // 10% deposit
+        }
+
+        private string GenerateReferenceNumber()
+        {
+            return "BK" + DateTime.Now.ToString("yyyyMMddHHmmss");
+        }
+
+        private void SaveBooking(int guestId, int roomNumber, DateTime checkIn, DateTime checkOut, decimal totalRate, decimal deposit, string referenceNumber)
+        {
+            var newBooking = hotelDatabaseDataSet.Booking.NewBookingRow();
+            newBooking.GuestID = guestId;
+            newBooking.RoomNumber = roomNumber;
+            newBooking.CheckInDate = checkIn;
+            newBooking.CheckOutDate = checkOut;
+            newBooking.TotalRate = totalRate; // Map to TotalRate field
+            hotelDatabaseDataSet.Booking.AddBookingRow(newBooking);
+            bookingTableAdapter.Update(hotelDatabaseDataSet.Booking);
+        }
+
+        private void ShowError(string message)
+        {
+            errorLabel.Text = message;
+        }
     }
 }
 
