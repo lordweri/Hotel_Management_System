eee0c881f817b0d6440261e2e3b96f4a3486f14e
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-09-30 21:29:58 +0200

    Added BookingController, RoomController and GuestController into the business layer
    Use these class to communicate with the database

---
+++ HotelSystem/HotelSystem/Business/GuestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelSystem.Data;
+
+namespace HotelSystem.Business
+{
+    //Make use of this class to CRUD operation on the Guest table from the database
+    public class GuestController
+    {
+        #region Data members
+        GuestDB guestDB;
+        Collection<Guest> guests;     //stores all guests in a collection
+        #endregion
+
+        #region Properties
+        public Collection<Guest> AllGuests
+        {
+            get
+            {
+                return guests;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public GuestController()
+        {
+            guestDB = new GuestDB();
+            guests = guestDB.AllGuests;    //guestDB.AllGuests uses the get method of GuestDB class to get ALL guests from the database
+        }
+
+        #endregion
+
+        //TODO
+        #region Database communication
+
+
+    }
+}
---
+++ HotelSystem/HotelSystem/Business/RoomController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HotelSystem.Data;
+
+namespace HotelSystem.Business
+{
+    //Make use of this class to CRUD operation on the Room table from the database
+    public class RoomController
+    {
+        #region Data members
+        RoomDB roomDB;
+        Collection<Room> rooms;     //stores all rooms in a collection
+        #endregion
+
+        #region Properties
+        public Collection<Room> AllRooms
+        {
+            get
+            {
+                return rooms;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public RoomController()
+        {
+            roomDB = new RoomDB();
+            rooms = roomDB.AllRooms;    //roomDB.AllRooms uses the get method of RoomDB class to get ALL rooms from the database
+        }
+        #endregion
+
+        //Use these method change the availability of a room to True or False
+        #region Database communication
+        //This method updates room DataSet
+        public void DataMaintenance(Room aRoom, DB.DBOperation operation)
+        {
+            int index = 0;
+            roomDB.DataSetChange(aRoom, operation);   //add, edit or delete room in the DataSet
+            switch (operation)
+            {
+                case DB.DBOperation.Add:      //For the project we probably won't use the add function
+                    rooms.Add(aRoom);         //add a room to the Collection
+                    break;
+                case DB.DBOperation.Edit:
+                    index = FindIndex(aRoom);
+                    aRoom = rooms[index];     //update a room in the Collection, i.e. replace a old room with a new room
+                    break;
+                case DB.DBOperation.Delete:
+                    index = FindIndex(aRoom);
+                    rooms.RemoveAt(index);
+                    break;
+            }
+        }
+
+        //This method commit changes to the database
+        public bool FinalizeChanges(Room room, DB.DBOperation operation)
+        {
+            return roomDB.UpdateDataSource(room, operation);
+        }
+        #endregion
+
+        //Use these methods to search for a room
+        #region Search Method
+        //searches for a room in the collection by room number
+        public Room FindRoom(string roomNumber)
+        {
+            int index = 0;
+            bool found = (rooms[index].getRoomNo() == roomNumber);  //check if it is the first record
+            int count = rooms.Count;                       //counts the number of rooms in the collection. For our project, it should be 5
+
+            while ((!(found) && (index < rooms.Count - 1)))
+            {
+                index++;
+                found = (rooms[index].getRoomNo() == roomNumber);    //check if it is the next record
+            }
+            return rooms[index];
+        }
+
+        //This method finds the index of a room in the collection
+        public int FindIndex(Room room)
+        {
+            int index = 0;
+            bool found = false;
+            found = (room.getRoomNo() == rooms[index].getRoomNo());
+            while (!(found) & index < rooms.Count - 1)
+            {
+                index++;
+                found = (room.getRoomNo() == rooms[index].getRoomNo());
+            }
+            if (found)
+            {
+                return index;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+    }
+}
+}
