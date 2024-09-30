0244838595bc3a29a35be957f17195da9ff9bef9
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-09-30 14:45:20 +0200

    Added property methods in Guest class
    So that Data layer can have access to its data members

--- HotelSystem/HotelSystem/Business/Guest.cs
+++ HotelSystem/HotelSystem/Business/Guest.cs
@@ -8,11 +8,43 @@ namespace HotelSystem
 {
     public class Guest
     {
-        private string Name;
+        private string name;
         private string guestID;
         private string email;
         private string telephone;
         private string bookingID;
+
+        #region property methods
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string GuestID
+        {
+            get { return guestID; }
+            set { guestID = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Telephone
+        {
+            get { return telephone; }
+            set { telephone = value; }
+        }
+
+        public string BookingID
+        {
+            get { return bookingID; }
+            set { bookingID = value; }
+        }
+        #endregion
 
 
         public Guest(string name, string Email, string phone,string bID)
