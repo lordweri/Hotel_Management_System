57af7914ab222c8fec23a8843de7e9c65b5ed8ea
lordweri <werimasao@gmail.com>
2024-10-02 10:45:56 +0200

    Creatd the Login Form for security purposes

---
+++ HotelSystem/HotelSystem/Presentation/LogForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace HotelSystem.Presentation
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtUserId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserId.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtUserId.ForeColor = System.Drawing.Color.Gray;
+            this.txtUserId.Location = new System.Drawing.Point(385, 274);
+            this.txtUserId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(210, 22);
+            this.txtUserId.TabIndex = 0;
+            this.txtUserId.Text = "ID";
+            this.txtUserId.Enter += new System.EventHandler(this.txtUserId_Enter);
+            this.txtUserId.Leave += new System.EventHandler(this.txtUserId_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtPassword.ForeColor = System.Drawing.Color.Gray;
+            this.txtPassword.Location = new System.Drawing.Point(385, 333);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(210, 22);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSignIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignIn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSignIn.Location = new System.Drawing.Point(434, 390);
+            this.btnSignIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(112, 32);
+            this.btnSignIn.TabIndex = 2;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = false;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1009, 82);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(378, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 42);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Sign In ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1009, 754);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnSignIn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "LoginForm";
+            this.Text = "Sign In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSignIn;
+        private Panel panel1;
+        private Label label1;
+    }
+}
---
+++ HotelSystem/HotelSystem/Presentation/LogForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelSystem.Presentation
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            string userId = txtUserId.Text;
+            string password = txtPassword.Text;
+
+            if (!string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Sign-in successful!");
+            }
+            else
+            {
+                MessageBox.Show("Please enter your ID and password.");
+            }
+        }
+
+        private void txtUserId_Enter(object sender, EventArgs e)
+        {
+            if (txtUserId.Text == "ID")
+            {
+                txtUserId.Text = "";
+                txtUserId.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUserId_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserId.Text))
+            {
+                txtUserId.Text = "ID";
+                txtUserId.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                txtPassword.UseSystemPasswordChar = false;
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Gray;
+            }
+        }
+
+        private void pnlUserId_Paint(object sender, PaintEventArgs e)
+        {
+            DrawRoundedRectangle(e.Graphics, pnlUserId.ClientRectangle, 20, Color.LightGray);
+        }
+
+        private void pnlPassword_Paint(object sender, PaintEventArgs e)
+        {
+            DrawRoundedRectangle(e.Graphics, pnlPassword.ClientRectangle, 20, Color.LightGray);
+        }
+
+        private void DrawRoundedRectangle(Graphics g, Rectangle r, int d, Color color)
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddArc(r.X, r.Y, d, d, 180, 90);
+                path.AddArc(r.Right - d, r.Y, d, d, 270, 90);
+                path.AddArc(r.Right - d, r.Bottom - d, d, d, 0, 90);
+                path.AddArc(r.X, r.Bottom - d, d, d, 90, 90);
+                path.CloseFigure();
+
+                using (Pen pen = new Pen(color, 1))
+                {
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.DrawPath(pen, path);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
