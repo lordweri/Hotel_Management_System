626014de0b8e86cee38e1d39bb7e18cd32757e3f
lordweri <werimasao@gmail.com>
2024-10-02 10:51:40 +0200

    commit

--- HotelSystem/HotelSystem/Presentation/LogForm.Designer.cs
+++ HotelSystem/HotelSystem/Presentation/LogForm.Designer.cs
@@ -98,7 +98,7 @@ namespace HotelSystem.Presentation
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1009, 754);
+            this.ClientSize = new System.Drawing.Size(968, 567);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtUserId);
