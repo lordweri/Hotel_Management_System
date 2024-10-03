61125c7f1f920bbfe5ced3fee87912fd77ba756e
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-10-03 23:20:37 +0200

    Create Payment.cs

---
+++ HotelSystem/HotelSystem/Business/Payment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelSystem.Data;
+
+namespace HotelSystem.Business
+{
+    public class Payment
+    {
+        public string PaymentID { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; }
+        public string BookingID { get; set; }
+
+        public Payment(string paymentID, string bookingID, decimal amount, DateTime date)
+        {
+            PaymentID = paymentID;
+            BookingID = bookingID;
+            Amount = amount;
+            Date = date;
+        }
+
+
+
+    }
+}
