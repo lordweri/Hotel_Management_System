50642b7e6f93ff45015adefafccbde1e05835767
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-10-04 00:23:11 +0200

    Update GuestTypeForm
    coding of back and continue button should still be done

--- HotelSystem/HotelSystem/Presentation/GuestTypeForm.cs
+++ HotelSystem/HotelSystem/Presentation/GuestTypeForm.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HotelSystem.Business;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,26 +14,68 @@ namespace HotelSystem.Presentation
 {
     public partial class GuestTypeForm : Form
     {
+        #region Data Members
+        GuestController guestController;
+        Collection<Guest> guests;
+        #endregion
+
         public GuestTypeForm()
         {
             InitializeComponent();
+            guestController = new GuestController();
+            guests = new Collection<Guest>();
         }
 
+        
+        //When this button is clicked, the user is taken to the form(Registration Form) to register a new guest
         private void btnNewGuest_Click(object sender, EventArgs e)
         {
             // Logic to handle new guest reservation
-            MessageBox.Show("New Guest selected.");
-            // You can call the form for a new guest reservation here
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.Show();          // Show the registration form
+            this.Close();                     // Close the current form
         }
 
+        //Check if the guest is existing, if existing, proceed to the reservation form(BookForm), if not, display an error message and do nothing
         private void btnExistingGuest_Click(object sender, EventArgs e)
         {
             // Logic to handle existing guest reservation
-            MessageBox.Show("Existing Guest selected.");
-            // You can call the form for an existing guest reservation here
+
+            string guestIdEntered = guestIDtextBox.Text;
+            foreach (Guest guest in guests)
+            {
+                if (guest.getGuestID() == guestIdEntered)
+                {
+                    // Guest found
+                    MessageBox.Show("Guest found!");
+                    // You can call the form for an existing guest reservation here
+                    BookForm bookForm = new BookForm(guest);        // We found the guest, proceed to the reservation form
+                    bookForm.Show();                           // Show the reservation form
+                    this.Close();                              // Close the current form
+                    continue;
+                }
+                else
+                {
+                    // Guest not found, display an error message and do nothing
+                    // Reenter the guest ID or register as a new guest
+                    MessageBox.Show("Guest not found!");      
+                }
+            }
         }
 
+        //Back button
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuestTypeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Continue button
+        private void continueButton_Click(object sender, EventArgs e)
         {
 
         }
