25d895ad43394a33f2e5b8b761b3f356882f4010
ZHENTAOYE <ZHENTAOYE03@GMAIL.COM>
2024-10-04 02:41:08 +0200

    Revert "Fixed the GuestID textbox and BookingID textbox in"
    This reverts commit c7d0e9d0e1db7a4c9da329e694aa75026e31af9e.

--- HotelSystem/HotelSystem/Presentation/ChangeReserveForm.cs
+++ HotelSystem/HotelSystem/Presentation/ChangeReserveForm.cs
@@ -16,42 +16,5 @@ namespace HotelSystem.Presentation
         {
             InitializeComponent();
         }
-
-        private void TxtUserId_Enter(object sender, EventArgs e)
-        {
-            if (txtUserId.Text == "BookingID")
-            {
-                txtUserId.Text = "";
-                txtUserId.ForeColor = System.Drawing.Color.Black; 
-            }
-        }
-
-        private void TxtUserId_Leave(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtUserId.Text))
-            {
-                txtUserId.Text = "BookingID"; 
-                txtUserId.ForeColor = System.Drawing.Color.Gray; 
-            }
-        }
-
-        private void TextBox1_Enter(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "GuestID")
-            {
-                textBox1.Text = "";
-                textBox1.ForeColor = System.Drawing.Color.Black; 
-            }
-        }
-
-        private void TextBox1_Leave(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(textBox1.Text))
-            {
-                textBox1.Text = "GuestID"; 
-                textBox1.ForeColor = System.Drawing.Color.Gray; 
-            }
-        }
-
     }
 }
--- HotelSystem/HotelSystem/Properties/Settings.Designer.cs
+++ HotelSystem/HotelSystem/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HotelSystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,7 +27,7 @@ namespace HotelSystem.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\HotelDatabase" +
-            ".mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True")]
+            ".mdf;Integrated Security=True")]
         public string HotelDatabaseConnectionString {
             get {
                 return ((string)(this["HotelDatabaseConnectionString"]));
